15cbd00a9cd2d8f19d9c9e4b4f765d6e31baa2b4
Hugo Brandão <hugo.brandao@dtidigital.com.br>
2022-10-29 18:36:58 -0300

    use tflite

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 from flask import Flask, request
 import numpy as np
 import tflite_runtime.interpreter as tflite
@@ -11,17 +12,32 @@ def health_check():
 
 @app.route('/api/analysis', methods=['POST'])
 def fire_probability():
-    model = tflite.Interpreter(model_path='./TPF.tflite')
+    interpreter = tflite.Interpreter(model_path='./TPF.tflite')
+    interpreter.allocate_tensors()
 
     file = request.files['file']
 
     img = Image.open(file.stream)
-    img = img.resize((224,224),Image.ANTIALIAS)
+    img = img.resize((224, 224), Image.ANTIALIAS)
 
-    x = np.array(img)
-    x = np.expand_dims(x, axis=0) /255
+    processed_image = np.array(img)
+    processed_image = np.expand_dims(processed_image, axis = 0) / 255
+    processed_image = np.float32(processed_image)
 
-    classes = model.predict(x)
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Test the model on random input data.
+    input_shape = input_details[0]['shape']
+    interpreter.set_tensor(input_details[0]['index'], processed_image)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    classes = interpreter.get_tensor(output_details[0]['index'])
+    
     result = np.argmax(classes[0])==0, max(classes[0])
 
     return { 
